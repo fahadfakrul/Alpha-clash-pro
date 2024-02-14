e232add21bc7aabf7a3a8f6a97d6a558b70d3c58
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-02-14 12:41:29 +0600

    pages linked

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,33 @@
-function play(){
-    console.log('play start now');
+
+// to hide and show 
+
+// function play(){
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+//     // console.log(homeSection.classList);
+
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden');
+// }
+
+function continueGame(){
+    // generate a random alphabet
+    const alphabet = getARandomAlphabet();
+
 }
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
+    }
+
+    function getARandomAlphabet(){
+        const alphabetString='abcdefghijklmnopqrstuvwxyz';
+        const alphabets = alphabetString.split('');
+
+        const randomNumber = Math.random()*25;
+        const index = Math.round(randomNumber);
+        const alphabet = alphabets[index];
+        return alphabet;
+    }
