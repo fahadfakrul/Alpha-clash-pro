44ab7985562f2002e013c279ac21125464deec78
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-02-14 23:01:45 +0600

    get and set score/life func created

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,5 +1,4 @@
-
-// to hide and show 
+// to hide and show
 
 // function play(){
 //     const homeSection = document.getElementById('home-screen');
@@ -10,42 +9,54 @@
 //     playgroundSection.classList.remove('hidden');
 // }
 
-function handleKeyboardKeyUpEvent(event){
-    const playerPressed = event.key;
-    // console.log('player pressed', playerPressed);
+function handleKeyboardKeyUpEvent(event) {
+  const playerPressed = event.key;
+  // console.log('player pressed', playerPressed);
 
-    // get the expected to press
-    const currentAlphabetElement = document.getElementById('current-alphabet');
-    const currentAlphabet = currentAlphabetElement.innerText;
-    const expectedAlphabet = currentAlphabet.toLowerCase();
+  // get the expected to press
+  const currentAlphabetElement = document.getElementById("current-alphabet");
+  const currentAlphabet = currentAlphabetElement.innerText;
+  const expectedAlphabet = currentAlphabet.toLowerCase();
 
-    // check matched or not
-    if(playerPressed === expectedAlphabet){
-        console.log('You got a point');
-        console.log('You have pressed correctly',expectedAlphabet);
-        removeBackgroundColorById(expectedAlphabet);
-        continueGame();
-    }else{
-        console.log('You not got a point')
-    }
-}
-//capture keyboard press 
-document.addEventListener('keyup',handleKeyboardKeyUpEvent);
+  // check matched or not
+  if (playerPressed === expectedAlphabet) {
+    console.log("You got a point");
+    // console.log('You have pressed correctly',expectedAlphabet);
+    const currentScoreElement = document.getElementById("current-score");
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScore = parseInt(currentScoreText);
 
-function continueGame(){
-    // generate a random alphabet
-    const alphabet = getARandomAlphabet();
-    const currentAlphabetElement = document.getElementById('current-alphabet');
-    currentAlphabetElement.innerText = alphabet;
+    const newScore = currentScore + 1;
 
-    setBackgroundColorById(alphabet);
+    currentScoreElement.innerText = newScore;
 
-}
-
-function play(){
-    hideElementById('home-screen');
-    showElementById('play-ground');
+    removeBackgroundColorById(expectedAlphabet);
     continueGame();
-    }
+  } else {
+    console.log("You not got a point");
+    const currentLifeElement = document.getElementById("current-life");
+    const currentLifeText = currentLifeElement.innerText;
+    const currentLife = parseInt(currentLifeText);
 
-    
+    const newLife = currentLife - 1;
+
+    currentLifeElement.innerText = newLife;
+  }
+}
+//capture keyboard press
+document.addEventListener("keyup", handleKeyboardKeyUpEvent);
+
+function continueGame() {
+  // generate a random alphabet
+  const alphabet = getARandomAlphabet();
+  const currentAlphabetElement = document.getElementById("current-alphabet");
+  currentAlphabetElement.innerText = alphabet;
+
+  setBackgroundColorById(alphabet);
+}
+
+function play() {
+  hideElementById("home-screen");
+  showElementById("play-ground");
+  continueGame();
+}
