c4f909b5eb260852976e80b7cc4836efc87c2700
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-02-19 22:48:54 +0600

    sound and some other features added

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -8,13 +8,18 @@
 //     const playgroundSection = document.getElementById('play-ground');
 //     playgroundSection.classList.remove('hidden');
 // }
+let isGamePlayOn = false;
 
+const artBoard = document.getElementById("artboard");
+const modalBox = document.getElementById("modal-box");
 function handleKeyboardKeyUpEvent(event) {
+  if (isGamePlayOn === false) return;
   const playerPressed = event.key;
   // console.log('player pressed', playerPressed);
 
+  const audio = new Audio();
 
-  if(playerPressed === 'Escaped'){
+  if (playerPressed === "Escape") {
     gameOver();
   }
   // get the expected to press
@@ -24,13 +29,14 @@ function handleKeyboardKeyUpEvent(event) {
 
   // check matched or not
   if (playerPressed === expectedAlphabet) {
-      console.log("You got a point");
-      // console.log('You have pressed correctly',expectedAlphabet);
-      
-      const currentScore = getTextElementValueById('current-score');
-      const updatedScore =currentScore + 1;
-      setTextElementValueById('current-score', updatedScore);
-      
+    console.log("You got a point");
+    // console.log('You have pressed correctly',expectedAlphabet);
+    audio.src = "../audios/right.mp3";
+    audio.play();
+    const currentScore = getTextElementValueById("current-score");
+    const updatedScore = currentScore + 1;
+    setTextElementValueById("current-score", updatedScore);
+
     // update score
     // const currentScoreElement = document.getElementById("current-score");
     // const currentScoreText = currentScoreElement.innerText;
@@ -43,13 +49,16 @@ function handleKeyboardKeyUpEvent(event) {
     removeBackgroundColorById(expectedAlphabet);
     continueGame();
   } else {
+    audio.src = "../audios/wrong.mp3";
+    audio.play();
+    const currentLife = getTextElementValueById("current-life");
+    const updatedLife = currentLife - 1;
+    const updatedLifePercentage = (updatedLife / 5) * 100;
+    artBoard.style.background = `linear-gradient(#FFFFFFB3 ${updatedLifePercentage}%,red)`;
+    setTextElementValueById("current-life", updatedLife);
 
-    const currentLife = getTextElementValueById('current-life');
-    const updatedLife =currentLife - 1;
-    setTextElementValueById('current-life', updatedLife);
-
-    if(updatedLife === 0){
-        gameOver();
+    if (updatedLife === 0) {
+      gameOver();
     }
 
     // console.log("You not got a point");
@@ -75,25 +84,38 @@ function continueGame() {
 }
 
 function play() {
-    // hide everthing and set to pg
-  hideElementById('home-screen');
-  hideElementById('final-score');
-  showElementById('play-ground');
-  
-  setTextElementValueById('current-life', 5);
-  setTextElementValueById('current-score', 0);
+  isGamePlayOn = true;
+  // hide everthing and set to pg
+  hideElementById("home-screen");
+  hideElementById("final-score");
+  showElementById("play-ground");
+
+  setTextElementValueById("current-life", 5);
+  setTextElementValueById("current-score", 0);
 
   continueGame();
 }
 
-function gameOver(){
-    hideElementById('play-ground');
-    showElementById('final-score');
-    // update final score
-    const lastScore = getTextElementValueById('current-score');
-    setTextElementValueById('last-score', lastScore);
+function gameOver() {
+  hideElementById("play-ground");
+  showElementById("final-score");
+  // update final score
+  const lastScore = getTextElementValueById("current-score");
+  setTextElementValueById("last-score", lastScore);
 
-    // clear selected alphabet at last
-    const currentAlphabet = getElementTextById('current-alphabet');
-    removeBackgroundColorById(currentAlphabet);
-} 
+  // clear selected alphabet at last
+  const currentAlphabet = getElementTextById("current-alphabet");
+  removeBackgroundColorById(currentAlphabet);
+  isGamePlayOn = false;
+  artBoard.style.background = "linear-gradient(#FFFFFFB3 100% , red)";
+}
+
+function modalOpen(event) {
+  if (event.clientY < 20) {
+    modalBox.style.display = "flex";
+  }
+}
+function modalClose(event) {
+  modalBox.style.display = "none";
+}
+document.body.onmousemove = modalOpen;
